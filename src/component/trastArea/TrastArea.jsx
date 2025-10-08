3983756f81b384d9d2490cee7a488045eb198b61
mirajul <mirajulbp452@gmail.com>
2025-10-09 01:55:46 +0600

    Trast Area done

---
+++ src/component/trastArea/TrastArea.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./TrastArea.css"
+
+export default function TrastArea() {
+  return (
+    <div className='bg-[#7039E6]'>
+        <div className='max-w-7xl mx-auto text-center py-8 text-white'>
+            <h2 className='text-3xl text-bold'>Trusted by Millions, Built for You</h2>
+            <div className='grid grid-cols-3 gap-6 justify-center items-center py-8'>
+                <div>
+                    <p className='text-[16px]'>Total Downloads</p>
+                    <h2 className='text-6xl font-bold my-3'>29.6M</h2>
+                    <p className='text-[16px]'>21% more than last month</p>
+                </div>
+                <div>
+                    <p className='text-[16px]'>Total Reviews</p>
+                    <h2 className='text-6xl font-bold my-3'>906K</h2>
+                    <p className='text-[16px]'>46% more than last month</p>
+                </div>
+                <div>
+                    <p className='text-[16px]'>Active Apps</p>
+                    <h2 className='text-6xl font-bold my-3'>132+</h2>
+                    <p className='text-[16px]'>31 more will Launch</p>
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../../component/banner/Banner'
+import TrastArea from '../../component/trastArea/TrastArea'
 
 export default function Home() {
   return (
     <div>
         <Banner></Banner>
+        <TrastArea></TrastArea>
     </div>
   )
 }
