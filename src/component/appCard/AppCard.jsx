cbb08a3166d6c9a496bbd1e4f59411280d5fd5e2
mirajul <mirajulbp452@gmail.com>
2025-10-17 11:29:23 +0600

    Search functionallty done

--- src/component/appCard/AppCard.jsx
+++ src/component/appCard/AppCard.jsx
@@ -3,21 +3,28 @@ import downloadImg from "../../assets/images/icon-downloads.png"
 import starImg from "../../assets/images/icon-ratings.png"
 
 export default function AppCard({app}) {
-    console.log(app)
+    // console.log(app)
   return (
-    <div className="card bg-base-200 p-4 w-72 gap-3 shadow-sm hover:scale-105 ease-in-out transition">
-        <figure>
-            <img className='w-[200px]'
-            src={app.image}
-            alt="appImg" />
-        </figure>
-        <div className="">
-            <h2 className="card-title">{app.title}</h2>
-            <div className="flex justify-between">
-                <button className="btn bg-gray-200 mt-3 font-bold text-[#00D390]"><img className='w-4' src={downloadImg}></img>{app.downloads} M</button>
-                <button className="btn bg-amber-100 mt-3 font-bold text-[#FE8A17]"><img className='w-4 text-[#FE8A17]' src={starImg}></img>{app.reviews}</button>
+    // <div className='max-w-7xl mx-auto'>
+    //     <div className='flex justify-between my-3'>
+    //         <h2 className='font-bold text-[48px] '>All Apps</h2>
+    //         <input type="text" placeholder='Search' />
+    //     </div>
+        <div className="card bg-base-200 p-4 w-72 gap-3 shadow-sm hover:scale-105 ease-in-out transition">
+            <figure>
+                <img className='w-[200px]'
+                src={app.image}
+                alt="appImg" />
+            </figure>
+            <div className="">
+                <h2 className="card-title">{app.title}</h2>
+                <div className="flex justify-between">
+                    <button className="btn bg-gray-200 mt-3 font-bold text-[#00D390]"><img className='w-4' src={downloadImg}></img>{app.downloads} M</button>
+                    <button className="btn bg-amber-100 mt-3 font-bold text-[#FE8A17]"><img className='w-4 text-[#FE8A17]' src={starImg}></img>{app.reviews}</button>
+                </div>
             </div>
         </div>
-    </div>
+
+    //  </div>
   )
 }
--- src/pages/apps/Apps.jsx
+++ src/pages/apps/Apps.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useApps from '../../hooks/useApps';
 import AppCard from '../../component/appCard/AppCard';
 
 export default function Apps() {
   const {apps} = useApps();
+  const [search, setSearch] = useState('')
+  const turm = search.trim().toLocaleLowerCase()
+  const searchApps = turm? apps.filter((app) => app.title.toLocaleLowerCase()
+  .includes(turm)):apps
+
+
+  
   return (
-      <div className='pb-28'>
+      <div className='pb-28 pt-6'>
                 <div className='flex justify-between my-3 max-w-7xl mx-auto'>
-                  <h2 className='font-bold text-[48px] '>All Apps</h2>
-                  <input type="text" placeholder='Search' />
+                  <h2 className='font-bold text-[48px] '>All Apps <span className='text-[25px]'>({searchApps.length})</span></h2>
+                  <label className="input mt-2">
+                    <input value ={search} onChange ={(e)=>setSearch(e.target.value)}  type="search" placeholder="Search Apps" />
+                  </label>  
                 </div>
                 <div className='mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-1 gap-y-7 max-w-7xl mx-auto'>
                   {
-                    apps.map (app => (
+                    searchApps.map (app => (
                       <AppCard key ={app.id} app ={app}></AppCard>
                     ))
                   }
